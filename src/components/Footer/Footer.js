2239285390342186741577bc8cd1bfca2ba06df9
Mahmoud Miz <mahmoudmaiz@icloud.com>
2021-07-17 03:02:01 -0300

    added list details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ import * as styles from "./styles.module.scss";
 // markup
 const Footer = () => {
   return (
-    <footer id="footer" className="headerText">
+    <footer id="footer" className={styles.footer}>
       footer foooter
     </footer>
   );
