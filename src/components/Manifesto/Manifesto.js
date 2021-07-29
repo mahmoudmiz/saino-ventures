05958d40f0353d7c4289f32771fcd2c234f48a9a
Mahmoud Miz <mahmoudmaiz@icloud.com>
2021-07-28 23:37:18 -0300

    added manifisto

--- src/components/Manifesto/Manifesto.js
+++ src/components/Manifesto/Manifesto.js
@@ -6,7 +6,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import * as styles from "./styles.module.scss";
 
 // assets
-import placeholder from "../../images/placeholder.png";
+import SAINO_ILLUSTRATION from "../../images/SAINO_ILLUSTRATION.svg";
 
 // markup
 const Manifesto = () => {
@@ -26,30 +26,52 @@ const Manifesto = () => {
             contract ayant un véritable impact et un pouvoir disruptif
             indéniable.
           </p>
-          <span className={styles.textbg}>manifesto</span>
         </div>
-        <img
-          src={placeholder}
-          className={styles.manifestoAsset}
-          alt="manifestoAsset"
-        />
+        <div className={styles.contentWrapper}>
+          <img
+            src={SAINO_ILLUSTRATION}
+            className={styles.manifestoAsset}
+            alt="manifestoAsset"
+          />
+          <div className={styles.rightWrapper}>
+            <div className={styles.manifestoBlock}>
+              <h5>Manifesto</h5>
+              <h3>Lorem ipsum dolor</h3>
+              <p>
+                A l’heure actuelle, nous en sommes encore au stade du
+                développement des protocoles des futures DLT de demain qui
+                devront potentiellement constituer des normes à l’échelles
+                internationales. Nous investissons donc principalement sur des
+                projets de type protocole. Nous estimons également que la DeFi
+                (finance décentralisé) ainsi que les plateformes NFTs sont les
+                premières applications des smart contract ayant un véritable
+                impact et un pouvoir disruptif indéniable.
+              </p>
+            </div>
 
-        <div className={styles.manifestoBlock}>
-          <h5>Manifesto</h5>
-          <h3>Lorem ipsum dolor</h3>
-          <p>
-            A l’heure actuelle, nous en sommes encore au stade du développement
-            des protocoles des futures DLT de demain qui devront potentiellement
-            constituer des normes à l’échelles internationales. Nous
-            investissons donc principalement sur des projets de type protocole.
-            Nous estimons également que la DeFi (finance décentralisé) ainsi que
-            les plateformes NFTs sont les premières applications des smart
-            contract ayant un véritable impact et un pouvoir disruptif
-            indéniable.
-          </p>
+            {/* only desktop*/}
+            <div className={styles.desktopContainer}>
+              <div className={styles.carouselItem}>
+                <h3>DLT</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+              </div>
+              <div className={styles.carouselItem}>
+                <h3>DLT</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className={styles.container}>
+        {/* only mobile*/}
+        <div className={styles.mobileContainer}>
           <Carousel selectedItem={0} showThumbs={false}>
             <div className={styles.carouselItem}>
               <h3>DLT</h3>
